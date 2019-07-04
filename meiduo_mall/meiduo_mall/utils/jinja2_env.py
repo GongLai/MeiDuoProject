e02c66d007b2e57a227cc75852ded3e25c232838
Hx176268 <19879631220@163.com>
2019-07-04 21:04:50 +0800

    美多商城--项目准备完成

---
+++ meiduo_mall/meiduo_mall/utils/jinja2_env.py
@@ -0,0 +1,13 @@
+from jinja2 import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+
+
+def jinja2_environment(**options):
+    env = Environment(**options)
+    env.globals.update({
+        'static': staticfiles_storage.url,
+        'url': reverse,
+    })
+    return env
+
